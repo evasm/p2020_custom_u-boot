bbe630295ec4e87374de0e52c798d4279d26bee2
Seungmin Chae <seungmin.chae@doosan.com>
2018-03-23 18:13:55 +0900

    need to fix net initial

---
+++ include/libfdt_env.h
@@ -0,0 +1,5 @@
+#ifdef USE_HOSTCC
+#include "../scripts/dtc/libfdt/libfdt_env.h"
+#else
+#include <linux/libfdt_env.h>
+#endif
---
+++ p2020_test/include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/freescale/p1_p2_rdb_pc
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/p1_p2_rdb_pc.h>
+#include <asm/config.h>
+#include <linux/kconfig.h>
+#include <config_fallbacks.h>
